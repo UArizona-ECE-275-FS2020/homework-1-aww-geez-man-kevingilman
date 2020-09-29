4714e7a41d7aadd3d1852b904741e23194927341
kevingilman <70494170+kevingilman@users.noreply.github.com>
2020-09-28 17:38:07 -0700

    Update AwwGeezMan.cpp

--- src/AwwGeezMan.cpp
+++ src/AwwGeezMan.cpp
@@ -21,6 +21,10 @@ exists a Morty who says everything backwards.
 
 // Include the Morty header file
 #include "Morty.hpp"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 
 
 int main (int argc, char** argv) {
